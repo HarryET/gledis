8318ba94d739c6de4374d7b1df5a0f868140c10a
Harry Bairstow <me@harryet.xyz>
2023-02-27 20:28:28 +0000

    feat: connect with opts

--- src/gledis_ffi.erl
+++ src/gledis_ffi.erl
@@ -1,5 +1,5 @@
 -module(gledis_ffi).
--export([null/0, coerce/1, connect/0, close/1, query/2]).
+-export([null/0, coerce/1, connect/0, connect_with_opts/1, close/1, query/2]).
 
 null() ->
     null.
@@ -8,6 +8,7 @@ coerce(Value) ->
     Value.
 
 connect() -> eredis:start_link().
+connect_with_opts(Options) -> eredis:start_link(Options).
 
 close(pid) -> eredis:stop(pid).
 
